0f8f41fbff260c9b8c977d9e99d1b6ed40d9c379
zhangyongquan <greenfish0626@gmail.com>
2021-07-30 15:39:06 +0800

    add VeeValid add config

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Card></Card>
   </div>
 </template>
+
+<script>
+import Card from '@/components/Card.vue'
+// @ is alias to /src
+export default {
+  components: {
+    Card
+  }
+}
+</script>
